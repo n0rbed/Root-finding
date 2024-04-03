c9eac50300cca9a23f3f2d21d2707d6868017eee
n0rbed <yassinahmad789@gmail.com>
2024-04-03 14:20:14 +0200

    draft of groebner alg

--- src/main.jl
+++ src/main.jl
@@ -1,7 +1,7 @@
 using Symbolics
 
 coeff = Symbolics.coeff
-function solve_poly(expression, x)
+function solve(expression, x)
     if isequal(SymbolicUtils.operation(expression.val), ^) && SymbolicUtils.arguments(expression.val)[2] isa Int64
         expression = SymbolicUtils.arguments(expression.val)[1]
     end
@@ -41,7 +41,7 @@ function solve_poly(expression, x)
     end
 end
 
-function solve_poly(polys::Vector, x)
+function solve(polys::Vector, x::Num)
     polys = unique(polys)
     expression = polys[1]
     for i = 2:(length(polys))
@@ -49,9 +49,49 @@ function solve_poly(polys::Vector, x)
     end
     expression = expand(expression)
     expression = simplify.(expression)
-    return solve_poly(expression, x)
+    return solve(expression, x)
 end
 
-@variables x a
-ex = (x^2 + 2*x + 1)^20
-println("x --> ", solve_poly(ex, x))
+
+function solve(eqs::Vector{Num}, vars::Vector{Num})
+    eqs = Symbolics.groebner_basis(eqs)
+    all_roots = Dict()
+
+    # initialize the place of each var
+    for var in vars
+        all_roots[var] = nothing
+    end
+
+
+    #get roots for first var (z in this case)
+    for eq in eqs
+        for var in vars
+            present_vars = Symbolics.get_variables(eq)
+            if isequal(var, present_vars[1]) && size(present_vars, 1) == 1 
+                var = Symbolics.get_variables(eq)[1]
+                all_roots[var] = solve(Symbolics.wrap(eq), var)
+            end
+        end
+    end
+
+    
+    # sub z, this is really messy and not systematic
+    # as changing the number of vars will force me to
+    # write more code, so i should think
+    # of a better way to design this
+    for (var, roots) in all_roots
+        if !isnothing(roots)
+            for eq in eqs
+                if var in Symbolics.get_variables(eq)
+                    substitute(eq, Dict([var => roots]))
+                end
+            end
+        end
+    end
+end
+
+@variables x y z
+equations = [x*y + z - 40, x*z + y - 51, x + y + z - 19]
+
+expr = [x^2 - 3*x - 10, x^2 + 20*x + 36]
+println("x --> ", roots(expr, x))
