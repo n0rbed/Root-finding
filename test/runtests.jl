a90b7ded1e54372a6826af8b288717fc84f248e1
n0rbed <yassinahmad789@gmail.com>
2024-06-04 23:57:39 +0300

    some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,20 @@
-using Test, RootFinding, Symbolics
+using RootFinding, Symbolics
+using Test
 
-@variables x
+@variables x y z
 
-@test isequal(solvepoly(x - 1, x), 1)
+@test isequal(solve(x+1, x), -1)
 
+@test isequal(solve(2x+1, x), -1/2)
+
+@test isequal(solve(x, x), 0) 
+
+@test isequal(solve(x^2 + 1, x), [-1, -1]) 
+
+@test isequal(solve(x^2 + 2x + 10) ,[-1 + 3im, -1 - 3im])
+
+@test isequal(solve(x^2 - 10x + 25) ,[5, 5])
+
+@test isequal(solve((x+1)^20, x), -1)
+
+@test isequal(solve(x^3 - 2x^2 + x - 2, x), [2, -im, im])
